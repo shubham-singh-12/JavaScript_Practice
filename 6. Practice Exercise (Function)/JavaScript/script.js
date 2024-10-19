79ae237f38e987350c45a3bb8475c3dee20c98a0
shubham-singh-12 <shubhamytsingh@gmail.com>
2024-10-19 19:32:39 +0530

    Latest commit

---
+++ 6. Practice Exercise (Function)/JavaScript/script.js
@@ -0,0 +1,25 @@
+// Solution 1
+function isOdd(number) {
+  let rem = number % 2;
+  let isOdd = rem == 1;
+  return isOdd;
+}
+
+console.log(isOdd(4));
+console.log(isOdd(5));
+console.log(isOdd(0));
+console.log(isOdd(9999));
+
+
+
+
+// Solution 2
+
+
+
+
+
+// Solution 3
+
+
+
