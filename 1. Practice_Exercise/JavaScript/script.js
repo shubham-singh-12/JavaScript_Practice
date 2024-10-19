4da67e9e0fed8a680a6c243a2f3ca6669e84d0f5
shubham-singh-12 <shubhamytsingh@gmail.com>
2024-10-19 23:13:06 +0530

    Revised 1st & 2nd Practice Exercise

--- 1. Practice_Exercise/JavaScript/script.js
+++ 1. Practice_Exercise/JavaScript/script.js
@@ -1,8 +1,12 @@
-alert("Good Morning");
+// Solution 1
+alert("Good Morning.");
 
-alert("Shubham Singh");
+// Solution 2
+alert("My name is: ");
 
+// Solution 3
 console.log(75 - 25);
 console.log(3 + 3 - 5);
 
+// Solution 4
 document.body.innerHTML = "I am Learning JS";
