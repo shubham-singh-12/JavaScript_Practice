dd9674aa9bdc9932f919994c699f698a509ed534
shubham-singh-12 <shubhamytsingh@gmail.com>
2024-10-20 09:42:53 +0530

    Practice Exercise 2 Updated

--- 2. Practice_Exercise/JavaScript/script.js
+++ 2. Practice_Exercise/JavaScript/script.js
@@ -1,21 +1,25 @@
 // Solution 1
-let dalPrice = 100;
-let rotiPrice = 10;
-let icecreamPrice = 30;
+const dal = 100;
+const roti = 10;
+const iceCream = 30;
 
-let finalBill = (dalPrice + (2 * (rotiPrice)) + icecreamPrice);
-// alert(`The final bill amount is: ${finalBill}`);
-// console.log(`The final bill amount is: ${finalBill}`);
-// document.body.innerHTML = 'The Final bill amount is: ₹' + finalBill;
+const finalBillAmount = ((1 * dal) + (2 * roti) + (1 * iceCream));
+
+// console.log(`Final Bill Amount is ₹${finalBillAmount}`);
+// alert(`Final Bill Amount is ₹${finalBillAmount}`);
+// document.body.innerHTML = `Final Bill Amount is ₹${finalBillAmount}`;
 
 
 
 // Solution 2
-let iPhone = 79990;
-let airPods = 24990;
+const iPhone15 = 79990;
+const airPodsPro = 24990;
 
-let totalPrice = ((iPhone + (2 * (airPods))) * 18 / 100);
-// document.body.innerHTML = `Total GST is: ${totalPrice}`;
+const totalPrice = (((1 * iPhone15) + (2 * airPodsPro)) * 18 / 100);
+
+// console.log(`18% GST is ₹${totalPrice}`);
+// alert(`18% GST is ₹${totalPrice}`);
+// document.body.innerHTML = `18% GST is ₹${totalPrice}`;
 
 
 
@@ -27,21 +31,27 @@ let totalPrice = ((iPhone + (2 * (airPods))) * 18 / 100);
 
 
 // Solution 4
-let firstName = " Shubham ";
-let lastName = "Singh ";
-let myNumber = 20;
+const firstName = ' Shubham ';
+const lastName = 'Singh ';
+const myNumber = 20;
 
-let fullName = firstName + lastName;
-let secondNumber = firstName + myNumber;
+const fullName = (firstName + lastName);
+const secondNumber = (firstName + myNumber);
 
-// alert(`My name is ${fullName} and with number ${secondNumber}`);
+// console.log(`String with String is ${fullName}`);
+// alert(`String with String is ${fullName}`);
+// document.body.innerHTML = `String with String is ${fullName}`;
+
+// console.log(`String with Number is ${secondNumber}`);
+// alert(`String with Number is ${secondNumber}`);
+// document.body.innerHTML = `String with Number is ${secondNumber}`;
 
 
 
 // Solution 5
-document.body.innerHTML = (`Your final Myntra Bag amount is: ₹ ${Math.round(((15599 - (10 / 100 * 15599)) - 272 + (2499 - (41 / 100 * 2499)) - 29 + 25 + 20) * 118 / 100)}`);
+// document.body.innerHTML = (`Your final Myntra Bag amount is: ₹ ${Math.round(((15599 - (10 / 100 * 15599)) - 272 + (2499 - (41 / 100 * 2499)) - 29 + 25 + 20) * 118 / 100)}`);
 
 
 
 // Solution 6
-alert(`Your final Myntra Bag amount is: ₹ ${Math.round(((15599 - (10 / 100 * 15599)) - 272 + (2499 - (41 / 100 * 2499)) - 29 + 25 + 20) * 118 / 100)}`);
+// alert(`Your final Myntra Bag amount is: ₹ ${Math.round(((15599 - (10 / 100 * 15599)) - 272 + (2499 - (41 / 100 * 2499)) - 29 + 25 + 20) * 118 / 100)}`);
