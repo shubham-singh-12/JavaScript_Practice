2e8566effc8f763188b8d4159b2f6cb89d15bd56
shubham-singh-12 <shubhamytsingh@gmail.com>
2024-11-01 18:37:47 +0530

    Updated Myntra Bag Exercise

--- Myntra_Bag_Exercise/JavaScript/script.js
+++ Myntra_Bag_Exercise/JavaScript/script.js
@@ -1,18 +1,24 @@
-let cartQuantity = 0; 
+let cartQuantity = 0;
 
-document.querySelector('#cart-summary').innerText = `Your Bag has ${cartQuantity} items.`;
+document.querySelector('#cart-summary').innerHTML = `Your Bag has ${cartQuantity} items.`;
 
+
+// Add to Bag Button
 function addToBag() {
- cartQuantity += 1;
- document.querySelector('#cart-summary').innerText = `Your Bag has ${cartQuantity} items.`;
+  cartQuantity += 1;
+  document.querySelector('#cart-summary').innerHTML = `Your Bag has ${cartQuantity} items.`;
 };
 
+
+// Move to Wishlist Button
 function wishlist() {
   cartQuantity -= 1;
-  document.querySelector('#cart-summary').innerText = `Your Bag has ${cartQuantity} items.`;
+  document.querySelector('#cart-summary').innerHTML = `Your Bag has ${cartQuantity} items.`;
 };
 
-function saleItem() {
+
+// Add 2 item Button (Sale Btton)
+function saleButton() {
   cartQuantity += 2;
-  document.querySelector('#cart-summary').innerText = `Your Bag has ${cartQuantity} items.`;
+  document.querySelector('#cart-summary').innerHTML = `Your Bag has ${cartQuantity} items.`;
 };
