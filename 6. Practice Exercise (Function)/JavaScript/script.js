8a01a0cbf67acf20da30874c60a3b6d9d9a185c4
shubham-singh-12 <shubhamytsingh@gmail.com>
2024-11-11 21:04:48 +0530

    Updated & Completed JavaScript file

--- 6. Practice Exercise (Function)/JavaScript/script.js
+++ 6. Practice Exercise (Function)/JavaScript/script.js
@@ -1,25 +1,40 @@
 // Solution 1
-function isOdd(number) {
-  let rem = number % 2;
-  let isOdd = rem == 1;
-  return isOdd;
-}
+// function checkOddEven(number) {
 
-console.log(isOdd(4));
-console.log(isOdd(5));
-console.log(isOdd(0));
-console.log(isOdd(9999));
+//   if(number % 2 === 0) {
+//     console.log(`${number} is Even.`);
+//   } else {
+//     console.log(`${number} is odd.`);
+//   }
+// }
+
+// checkOddEven(prompt('Enter Number to find Odd/Even: '));
 
 
 
 
 // Solution 2
+// function findLargestNumber(number1, number2) {
+//   number1 = prompt('Enter 1st number: ');
+//   number2 = prompt('Enter 2nd number: ');
 
+//   // if (number1 > number2) {
+//   //   console.log(`${number1} is greater than ${number2}`);
+//   // } else {
+//   //   console.log(`${number2} is greater than ${number1}`);
+//   // }
+
+//   return number1 > number2 ? `${number1} is greater than ${number2}` : `${number2} is greater than ${number1}`
+// }
+
+// console.log(findLargestNumber());
 
 
 
 
 // Solution 3
+function checkTemperature(celsiusValue) {
+  return 9/5 * celsiusValue + 32;
+}
 
-
-
+console.log(checkTemperature(prompt('Enter Degree to find Fahrenheit: ')));
